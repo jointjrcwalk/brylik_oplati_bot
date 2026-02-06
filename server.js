b8cd8d2cf26aa8748cb07141ebcabc97d22d2da8
jointjrcwalk <117079168+jointjrcwalk@users.noreply.github.com>
2026-02-06 16:43:22 +0200

    Update server.js
    fix port

--- server.js
+++ server.js
@@ -83,7 +83,6 @@ app.post('/webhook/monobank', async (req, res) => {
   res.sendStatus(200);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🌐 Webhook server running on port ${PORT}`);
-});
+app.listen(3000, () => {
+   console.log('Webhook server running on port 3000');
+ });
