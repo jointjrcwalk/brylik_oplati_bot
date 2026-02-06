8b0db1657d0c68b563cc6ed2f2429e9f6164a103
jointjrcwalk <117079168+jointjrcwalk@users.noreply.github.com>
2026-02-06 20:10:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -46,7 +46,6 @@ import 'dotenv/config';
    res.sendStatus(200);
  });
  
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('Webhook server running on port', PORT);
-});
+app.listen(3000, () => {
+   console.log('Webhook server running on port 3000');
+ });
