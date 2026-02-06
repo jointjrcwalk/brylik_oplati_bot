a26e31dd53e34980d128e91e7cd0183f70fa9a24
jointjrcwalk <117079168+jointjrcwalk@users.noreply.github.com>
2026-02-06 19:43:00 +0200

    Update monobank.js

--- monobank.js
+++ monobank.js
@@ -11,7 +11,7 @@ export async function createInvoice(amount) {
     },
     {
       headers: {
-        'X-Token': process.env.MONO_TOKEN,
+        'X-Token': process.env.MONO_TOKEN_TEST,
         'Content-Type': 'application/json'
       }
     }
